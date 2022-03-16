26e63e2bdc07532ea1057a70d53a3a5ea6806e9e
rajan navagare <rajan.v.navagare@gmail.com>
2022-03-16 11:12:19 +0530

    test2

--- Calculator/src/main/java/com/navagare/calculator/CalculatorApplication.java
+++ Calculator/src/main/java/com/navagare/calculator/CalculatorApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Application Started"+20);
+		System.out.println("Application Started"+30);
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
 }
